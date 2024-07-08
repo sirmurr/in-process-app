306e51ce452c559ce23f202acad11f5a0d711ec9
Kevin <cruzk346@gmail.com>
2024-07-08 18:04:35 -0400

    login screen

--- frontend/ipa/src/Login.js
+++ frontend/ipa/src/Login.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function Login() {
-  
+  fetch('http://127.0.0.1:8081/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
 
   return (
     <>
       <h1 className="Login-Screen">Login Splash Page</h1>
       <p>Select A User Profile</p>
+      {data && data.map(user => <p key={user.id}>{user.MemberName}</p>)}
+
     </>
   );
 }
