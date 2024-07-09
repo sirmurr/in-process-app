f4fa816f787b1ebc366666957f374a3f952808c2
Kevin <cruzk346@gmail.com>
2024-07-09 09:20:17 -0400

    login attempt 2

--- frontend/ipa/src/Login.js
+++ frontend/ipa/src/Login.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState, useNavigate } from 'react';
+
 
 function Login() {
-  fetch('http://127.0.0.1:8081/users', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
+  const [users, setUsers] = useState([]);
+  const [userId, setUserId] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    fetch('http://127.0.0.1:8081/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+      })
+      .catch(error => console.error('Error:', error));
+    if (users.isAdmin) {
+      navigate('/appadmin');
+    } else if (users.isTaskAdmin) {
+      setUserId(users.id);
+      navigate('/taskadmin/:id');
+    } else if (users.isLeadership) {
+      navigate('/leadership');
+    } else {
+      setUserId(users.id);
+      navigate('/newmember/:id');
     }
-  })
-    .then(response => response.json())
-    .then(data => console.log('Success:', data))
-    .catch(error => console.error('Error:', error));
+  }
 
   return (
     <>
       <h1 className="Login-Screen">Login Splash Page</h1>
       <p>Select A User Profile</p>
-      {data && data.map(user => <p key={user.id}>{user.MemberName}</p>)}
-
+      <select onClick={handleLogin} value={users && users.map(user => <p id="users" key={user.id}>  <img src='https://static.thenounproject.com/png/3874124-200.png' alt='user' /> {user.MemberName}</p>)} onChange={(e) => setUserId(e.target.value)}>
+      </select>
     </>
   );
 }
